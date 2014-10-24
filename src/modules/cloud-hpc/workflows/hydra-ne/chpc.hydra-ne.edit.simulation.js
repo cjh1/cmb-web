456baead246d31f9ee7c6c58fee2e64b3a9113e3
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2014-10-24 13:49:18 -0600

    Some sim input ui progress

--- src/modules/cloud-hpc/workflows/hydra-ne/chpc.hydra-ne.edit.simulation.js
+++ src/modules/cloud-hpc/workflows/hydra-ne/chpc.hydra-ne.edit.simulation.js
@@ -5,12 +5,46 @@ angular.module('chpc.workflow.hydra-ne')
         template: $templateCache.get('cloud-hpc/workflows/hydra-ne/chpc.hydra-ne.edit.simulation.html')
     };
 }])
+.directive('inputParameter', ['$templateCache', function ($templateCache) {
+  return {
+        scope: {
+          property: '='
+        },
+        restrict: 'A',
+        template: $templateCache.get('cloud-hpc/workflows/hydra-ne/chpc.hydra-ne.property.html')
+    };
+}])
 .controller('hydraNeEditSimulationCtrl', [ '$scope', 'girder.net.GirderConnector', function($scope, $girder) {
-   $scope.activeSection = 'mat';
+   var statusClasses = { complete: 'fa-check green', incomplete: 'fa-close red'},
+      sectionsStatus = {};
 
-   $scope.activate = function (sectionName) {
-      console.log('activate ' + sectionName);
-      $scope.activeSection = sectionName;
-   };
+  $scope.activeSection = 'mat';
+  $scope.inputTemplate = {};
+  $girder.fetch('assets/input-hydra.json')
+    .success(function(data) {
+      $scope.inputTemplate = data;
+      sectionsStatus = {};
+
+      var array = data.views,
+          count = array.length;
+      while(count--) {
+        sectionsStatus[array[count].id] = 'incomplete';
+      }
+    })
+    .error(function() {
+      console.log('error fetching def');
+    });
+
+  $scope.validate = function(sectionName) {
+    sectionsStatus[sectionName] = 'complete';
+  };
+
+  $scope.getClassIcon = function(sectionName) {
+    return statusClasses[sectionsStatus[sectionName]];
+  };
+
+  $scope.activate = function (sectionName) {
+    $scope.activeSection = sectionName;
+  };
 
 }]);
