47d25a8fe2bc1e2a1c0386b6e6839b818b86ed4a
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2014-10-28 08:49:42 -0600

    Fix minor style and code layout

--- src/modules/cloud-hpc/chpc.main.controller.js
+++ src/modules/cloud-hpc/chpc.main.controller.js
@@ -1,35 +1,4 @@
 angular.module('chpc.main')
-    .directive('notificationCenter', ['$templateCache', function ($templateCache) {
-        return {
-            controller: ['$scope', '$timeout', function ($scope, $timeout) {
-                var id = null;
-
-                $scope.message = '';
-                $scope.$on('notification-message', function (evt, message) {
-                    if (id) {
-                        $timeout.cancel(id);
-                    }
-
-                    $scope.message = message;
-
-                    id = $timeout(function () {
-                        $scope.message = null
-                    }, 3000);
-                });
-
-                $scope.percentage = function () {
-                    return 100 * $scope.message.done / $scope.message.total;
-                };
-
-                $scope.percentageInt = function () {
-                    return Math.floor($scope.percentage());
-                };
-            }],
-
-            //template: '<span ng-if="message">{{ message }}</span>'
-            template: $templateCache.get('cloud-hpc/notification.center.html')
-        };
-    }])
     /**
      * The chpc.main.RootController controller provide the root API
      * of the application workflow.
---
+++ src/modules/cloud-hpc/chpc.notification.directive.js
@@ -0,0 +1,32 @@
+angular.module('chpc.main')
+.directive('notificationCenter', ['$templateCache', function ($templateCache) {
+    return {
+        controller: ['$scope', '$timeout', function ($scope, $timeout) {
+            var id = null,
+                timeoutValue = 5000; // 5s
+
+            $scope.message = '';
+            $scope.$on('notification-message', function (evt, message) {
+                if (id) {
+                    $timeout.cancel(id);
+                }
+
+                $scope.message = message;
+
+                id = $timeout(function () {
+                    $scope.message = null;
+                }, timeoutValue);
+            });
+
+            $scope.percentage = function () {
+                return 100 * $scope.message.done / $scope.message.total;
+            };
+
+            $scope.percentageInt = function () {
+                return Math.floor($scope.percentage());
+            };
+        }],
+
+        template: $templateCache.get('cloud-hpc/chpc.notification.center.html')
+    };
+}]);
