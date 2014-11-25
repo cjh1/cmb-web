5189d37605fd5c00fb88593ff3313034bc73d9d1
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2014-11-24 22:09:13 -0700

    Fix JS syntax

--- src/modules/cloud-hpc/workflows/module.js
+++ src/modules/cloud-hpc/workflows/module.js
@@ -17,7 +17,7 @@ angular.module('chpc',
             var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
                 number = Math.floor(Math.log(bytes) / Math.log(1024));
             return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
-        }
+        };
     })
     .directive("fileread", [function () {
         return {
