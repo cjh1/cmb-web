ffb997b19b425af122e360547f1b4f7e158d7e23
Chris Harris <chris.harris@kitware.com>
2014-12-01 16:07:19 -0500

    Fix formatting, using PEP8

--- scripts/hydra-ne/pv_mesh_viewer.py
+++ scripts/hydra-ne/pv_mesh_viewer.py
@@ -6,7 +6,7 @@ import atexit
 import sys
 
 # import paraview modules.
-from paraview.web import wamp      as pv_wamp
+from paraview.web import wamp as pv_wamp
 from paraview.web import protocols as pv_protocols
 
 # import RPC annotation
@@ -26,113 +26,119 @@ except ImportError:
 # Create custom Pipeline Manager class to handle clients requests
 # =============================================================================
 
+
 class _MeshViewer(pv_wamp.PVServerProtocol):
 
-   meshFile = None
-   faces = None
+    meshFile = None
+    faces = None
 
-   @staticmethod
-   def add_arguments(parser):
-      parser.add_argument("--token", default=None, help="The Girder token for authentication", dest="token")
-      parser.add_argument("--url", default=None, help="The Girder base URL", dest="url")
-      parser.add_argument("--mesh", default=None, help="Surface Mesh file id to load", dest="file")
+    @staticmethod
+    def add_arguments(parser):
+        parser.add_argument(
+            "--token", default=None, help="The Girder token for authentication", dest="token")
+        parser.add_argument(
+            "--url", default=None, help="The Girder base URL", dest="url")
+        parser.add_argument(
+            "--mesh", default=None, help="Surface Mesh file id to load", dest="file")
 
+    @staticmethod
+    def configure(args):
+        _MeshViewer.authKey = args.authKey
+        _MeshViewer.meshFileId = args.file
+        _MeshViewer.url = args.url
+        _MeshViewer.token = args.token
 
-   @staticmethod
-   def configure(args):
-      _MeshViewer.authKey = args.authKey
-      _MeshViewer.meshFileId = args.file
-      _MeshViewer.url = args.url
-      _MeshViewer.token = args.token
+    def initialize(self):
+        # Bring used components
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebMouseHandler())
+        self.registerVtkWebProtocol(pv_protocols.ParaViewWebViewPort())
+        self.registerVtkWebProtocol(
+            pv_protocols.ParaViewWebViewPortImageDelivery())
 
-   def initialize(self):
-      # Bring used components
-      self.registerVtkWebProtocol(pv_protocols.ParaViewWebMouseHandler())
-      self.registerVtkWebProtocol(pv_protocols.ParaViewWebViewPort())
-      self.registerVtkWebProtocol(pv_protocols.ParaViewWebViewPortImageDelivery())
+        # Update authentication key to use
+        self.updateSecret(_MeshViewer.authKey)
 
-      # Update authentication key to use
-      self.updateSecret(_MeshViewer.authKey)
+        # Process file onlu once
+        if not _MeshViewer.faces:
+            self.processFile()
 
-      # Process file onlu once
-      if not _MeshViewer.faces:
-         self.processFile()
+    def _download_mesh_file(self):
+        mesh_file = tempfile.NamedTemporaryFile(suffix='.vtk', delete=False)
+        url = '%s/file/%s/download' % (_MeshViewer.url, _MeshViewer.meshFileId)
+        headers = {
+            'Girder-Token': _MeshViewer.token
+        }
+        r = requests.get(url, headers=headers)
 
-   def _download_mesh_file(self):
-      mesh_file = tempfile.NamedTemporaryFile(suffix='.vtk', delete=False)
-      url = '%s/file/%s/download' % (_MeshViewer.url, _MeshViewer.meshFileId)
-      headers = {
-         'Girder-Token': _MeshViewer.token
-      }
-      r = requests.get(url, headers=headers)
+        if r.status_code != 200:
+            print >> sys.stderr, r.json()
 
-      if r.status_code != 200:
-          print >> sys.stderr, r.json()
+        r.raise_for_status()
+        mesh_file.write(r.content)
+        mesh_file.close()
+        _MeshViewer.meshFile = mesh_file.name
 
-      r.raise_for_status()
-      mesh_file.write(r.content)
-      mesh_file.close()
-      _MeshViewer.meshFile = mesh_file.name
+        # register for cleanup
+        def _cleanup():
+            os.remove(_MeshViewer.meshFile)
 
-      # register for cleanup
-      def _cleanup():
-          os.remove(_MeshViewer.meshFile)
+        atexit.register(_cleanup)
 
-      atexit.register(_cleanup)
+    def processFile(self):
+        self._download_mesh_file()
 
-   def processFile(self):
-      self._download_mesh_file()
+        reader = simple.OpenDataFile(_MeshViewer.meshFile)
+        reader.UpdatePipeline()
 
-      reader = simple.OpenDataFile(_MeshViewer.meshFile)
-      reader.UpdatePipeline()
-
-      # Get information about cell data arrays
-      nbFaces = 0
-      cdInfo = reader.GetCellDataInformation()
-      numberOfCellArrays = cdInfo.GetNumberOfArrays()
-      for idx in xrange(numberOfCellArrays):
+        # Get information about cell data arrays
+        nbFaces = 0
+        cdInfo = reader.GetCellDataInformation()
+        numberOfCellArrays = cdInfo.GetNumberOfArrays()
+        for idx in xrange(numberOfCellArrays):
             array = cdInfo.GetArray(idx)
             if array.GetName() != 'modelfaceids':
-               continue
+                continue
             nbFaces = int(array.GetRange(-1)[1])
 
-      # Extract each face and keep representation around
-      _MeshViewer.faces = []
-      for idx in range(nbFaces):
-         threshold = simple.Threshold(Scalars = ['CELLS', 'modelfaceids'], Input=reader, ThresholdRange=[idx, idx])
-         rep = simple.Show(threshold)
-         _MeshViewer.faces.append(rep)
+        # Extract each face and keep representation around
+        _MeshViewer.faces = []
+        for idx in range(nbFaces):
+            threshold = simple.Threshold(
+                Scalars=['CELLS', 'modelfaceids'], Input=reader, ThresholdRange=[idx, idx])
+            rep = simple.Show(threshold)
+            _MeshViewer.faces.append(rep)
 
-      view = simple.Render()
-      view.Background = [0,0,0]
+        view = simple.Render()
+        view.Background = [0, 0, 0]
 
-   def convertToColor(self, colorStr):
-      encoding = "0123456789abcdef"
-      result = []
-      for colorIdx in range(3):
-         value = float(encoding.index(colorStr[colorIdx*2])*16 + encoding.index(colorStr[colorIdx*2 + 1])) / 255.0
-         result.append(value)
-      return result
+    def convertToColor(self, colorStr):
+        encoding = "0123456789abcdef"
+        result = []
+        for colorIdx in range(3):
+            value = float(encoding.index(
+                colorStr[colorIdx * 2]) * 16 + encoding.index(colorStr[colorIdx * 2 + 1])) / 255.0
+            result.append(value)
+        return result
 
-   @exportRpc('toggle.visibility')
-   def toggleVisibility(self, index, visible):
-      if index == -1:
-         for rep in _MeshViewer.faces:
-            rep.Visibility = visible
-      else:
-         _MeshViewer.faces[index].Visibility = visible
+    @exportRpc('toggle.visibility')
+    def toggleVisibility(self, index, visible):
+        if index == -1:
+            for rep in _MeshViewer.faces:
+                rep.Visibility = visible
+        else:
+            _MeshViewer.faces[index].Visibility = visible
 
-   @exportRpc('toggle.color')
-   def changeColor(self, index, color):
-      _MeshViewer.faces[index].DiffuseColor = self.convertToColor(color[1:])
+    @exportRpc('toggle.color')
+    def changeColor(self, index, color):
+        _MeshViewer.faces[index].DiffuseColor = self.convertToColor(color[1:])
 
-   @exportRpc('extract.faces')
-   def getFaceList(self):
-      # FIXME when we get the right face names
-      result = []
-      for i in range(len(_MeshViewer.faces)):
-         result.append("Face %d" % (i+1))
-      return result
+    @exportRpc('extract.faces')
+    def getFaceList(self):
+        # FIXME when we get the right face names
+        result = []
+        for i in range(len(_MeshViewer.faces)):
+            result.append("Face %d" % (i + 1))
+        return result
 
 # =============================================================================
 # Main: Parse args and start server
@@ -141,7 +147,6 @@ class _MeshViewer(pv_wamp.PVServerProtocol):
 if __name__ == "__main__":
     # Create argument parser
     parser = argparse.ArgumentParser(description="Mesh Viewer")
-
     # Add arguments
     server.add_arguments(parser)
     _MeshViewer.add_arguments(parser)
